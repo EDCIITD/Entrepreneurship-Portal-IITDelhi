14be5ad33174b28b2e4920e4117a80cdd7fa1908
Subhalingam D <subhalingam.d@gmail.com>
2019-10-12 15:31:59 +0530

    Header buggy
    Nav not right aligned

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/modernizr.js"></script>
+
+	<title>Student Incubation Cell</title>
+
+
+	<!-- IMPORT FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Lato&display=swap" rel="stylesheet">
+
+	<!-- ICONS !-->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css'>
+
+	<!-- JQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+	<!-- Menu -->
+	<script type="text/javascript">
+		function ToggleNav(){
+			$('ul.nav').slideToggle("slow");
+		}
+	</script>
+
+</head>
+
+<header>
+	<div id="toggle"><a onclick="$('ul.nav').slideToggle('slow');"><i class="material-icons" style="font-size:1.5em;color:#fff">menu</i></a></div>
+	<div class="logo"><img src="logo.png"></div>
+	<ul class="main-nav">
+		<li id="sec1"><a href="#" class="selected">Section 1</a></li>
+		<li id="sec2"><a href="#">Section 2</a></li>
+		<li id="sec3"><a href="#">Section 3</a></li>
+		<li id="sec4"><a href="#">Section 4</a></li>
+	</ul>
+</header>
+
+<body>
+	
+	<section class="cd-section visible">
+		<div>
+			<h2>Student Incubation Cell</h2>
+		</div>
+	</section>
+
+	<section class="cd-section">
+		<div>
+			<h2>About Us</h2>
+		</div>
+	</section>
+
+	<section class="cd-section">
+		<div>
+			<h2>Team</h2>
+		</div>
+	</section>
+
+	<section class="cd-section">
+
+			<h2>Incubation Spaces</h2>
+
+	</section>
+
+	<section class="cd-section">
+		<div>
+			<h2>Startups</h2>
+		</div>
+	</section>
+
+	<nav>
+		<ul class="cd-vertical-nav">
+			<li><a href="#0" class="cd-prev inactive">Next</a></li>
+			<li><a href="#0" class="cd-next">Prev</a></li>
+		</ul>
+	</nav> <!-- .cd-vertical-nav -->
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/velocity.min.js"></script>
+<script src="js/velocity.ui.min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+</body>
+</html>
